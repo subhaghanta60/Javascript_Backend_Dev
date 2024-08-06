65047dea5d544df4078573758ee10fb58d26173e
Subha Ghanta <subhaghanta60@gmail.com>
2024-08-06 11:45:42 +0530

    Error Handling && API Response Handle

--- Backend_Project/src/index.js
+++ Backend_Project/src/index.js
@@ -3,15 +3,24 @@ import dotenv from "dotenv";
 dotenv.config({
     path: '.env'
 });
-
+import express from "express";
 import connectDB from "./db/index.js";
 
-connectDB();
+const app = express();
+
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is Running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDb Connection Failed !!", err);
+})
 
 
 /*
-import express from "express";
-const app = express();
+
 
 ;( async ()=>{
     try {
