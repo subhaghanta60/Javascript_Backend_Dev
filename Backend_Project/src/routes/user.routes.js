c32188b5dedef13185e33659c12876ff281eabcd
Subha Ghanta <subhaghanta60@gmail.com>
2024-08-08 13:29:16 +0530

    Middleware, Cookies

--- Backend_Project/src/routes/user.routes.js
+++ Backend_Project/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middlewares.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middlewares.js";
+import {verifyJWT} from "../middlewares/auth.middlewares.js"
 const router = Router();
 
 router.route("/register").post(
@@ -17,6 +18,9 @@ router.route("/register").post(
     registerUser
 )
 
+router.route("/login").post (loginUser)
 
+//secured Routes
 
+router.route("/logout").post(verifyJWT,logoutUser)
 export default router
