06b09f50708b560619ef00d8829dba317aff5808
Subha Ghanta <subhaghanta60@gmail.com>
2024-08-10 14:09:04 +0530

    SubPipeline

--- Backend_Project/src/controllers/user.controller.js
+++ Backend_Project/src/controllers/user.controller.js
@@ -412,6 +412,64 @@ const getUserChannelProfile = asyncHandler(async(req,res)=> {
 
 });
 
+const getWatchHistory = asyncHandler(async(req,res)=> {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+                $lookup: {
+                    from: "videos",
+                    localField: "watchHistory",
+                    foreignField: "_id",
+                    as: "watchHistory",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "user",
+                                localField: "owner",
+                                foreignField:"_id",
+                                as: "VideoOwner",
+                                pipeline: [
+                                    {
+                                    $project: {
+                                                fullName: 1,
+                                                username:1,
+                                                avatar:1
+                                            }
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            $addFields: {
+                                owner:{
+                                    $first: "$videOwner"
+                                }
+                            }
+                        }
+                    ]
+                }
+         }
+        
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200 ,
+            user[0].watchHistory,
+            "Watch History Fetched Successfully"
+        
+        )
+    )
+
+})
+
 export {
     registerUser,
     loginUser,
@@ -421,5 +479,6 @@ export {
     gerCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
